454275f953c43d59bde9eac477df9cef99b27d1c
Velyn <9047311-Velyn@users.noreply.gitlab.com>
2022-07-06 04:51:51 -0700

    Add event fields to ZoneInPacket

--- xiffi/event.lua
+++ xiffi/event.lua
@@ -128,3 +128,5 @@ ffi.cdef[[
         u32 EventId : 24;
     } ReleasePacket;
 ]]
+
+--- also see ZoneInPacket
--- xiffi/zone.lua
+++ xiffi/zone.lua
@@ -15,23 +15,35 @@ ffi.cdef[[
 ]]
 
 ---@class ZoneInPacket
+---@field ZoneInEventFileId integer
+---@field ZoneInEventZoneId integer
+---@field ZoneInEventId integer
+---@field ZoneInEventFlags0 integer
 ---@field Weather integer
 ---@field PrevWeather integer
 ---@field VanaMinute integer
 ---@field PrevVanaMinute integer
 ---@field TransitionLength integer
 ---@field PrevTransitionLength integer
+---@field SubZoneId integer
 local ZoneInPacket = {}
 
 ffi.cdef[[
     typedef struct ZoneInPacket {
-        u8 unimplemented[0x68];
+        u8 unimplemented0[0x40];
+        u16 ZoneInEventFileId;
+        u8 unimplemented1[0x20];
+        u16 ZoneInEventZoneId;
+        u16 ZoneInEventId;
+        u16 ZoneInEventFlags0;
         u16 Weather;
         u16 PrevWeather;
         u32 VanaMinute;
         u32 PrevVanaMinute;
         u16 TransitionLength;
         u16 PrevTransitionLength;
+        u8 unimplemented2[0x9E - 0x78];
+        u16 SubZoneId;
     } ZoneInPacket;
 ]]
 
