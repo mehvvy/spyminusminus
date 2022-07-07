1fe6eedeb95df33a0431ef9955a888c8f39979dc
Velyn <9047311-Velyn@users.noreply.gitlab.com>
2022-07-07 00:34:21 -0700

    Add a prototype sketch of packet filtering

--- spymm.lua
+++ spymm.lua
@@ -5,74 +5,50 @@ local xiffi = require('xiffi/xiffi')
 local parsers = require('spymm/parsers')
 
 local iParsers = {
-    [0x00A] = function (row)
-        local p = xiffi.toZoneInPacket(row.data)
+    [0x00A] = parsers.zoneInPacket,
 
-        parsers.zoneInPacket(row, p)
-    end,
+    [0x00E] = parsers.entityUpdatePacket,
 
-    [0x00E] = function (row)
-        local p = xiffi.toEntityUpdatePacket(row.data)
+    [0x032] = parsers.eventPacket,
+    [0x033] = parsers.eventStringPacket,
+    [0x034] = parsers.eventParamPacket,
 
-        parsers.entityUpdatePacket(row, p)
-    end,
+    [0x052] = parsers.releasePacket,
 
-    [0x032] = function (row)
-        local p = xiffi.toEventPacket(row.data)
+    [0x057] = parsers.weatherPacket,
 
-        parsers.eventPacket(row, p)
-    end,
+    [0x05B] = parsers.positionPacket,
+    [0x05C] = parsers.eventUpdatePacket,
+    [0x05D] = parsers.eventUpdateStringPacket,
 
-    [0x033] = function (row)
-        local p = xiffi.toEventStringPacket(row.data)
-
-        parsers.eventStringPacket(row, p)
-    end,
-
-    [0x034] = function (row)
-        local p = xiffi.toEventParamPacket(row.data)
-
-        parsers.eventParamPacket(row, p)
-    end,
-
-    [0x052] = function (row)
-        local p = xiffi.toReleasePacket(row.data)
-
-        parsers.releasePacket(row, p)
-    end,
-
-    [0x057] = function (row)
-        local p = xiffi.toWeatherPacket(row.data)
-
-        parsers.weatherPacket(row, p)
-    end,
-
-    [0x05B] = function (row)
-        local p = xiffi.toPositionPacket(row.data)
-
-        parsers.positionPacket(row, p)
-    end,
-
-    [0x05C] = function (row)
-        local p = xiffi.toEventUpdatePacket(row.data)
-
-        parsers.eventUpdatePacket(row, p)
-    end,
-
-    [0x05D] = function (row)
-        local p = xiffi.toEventUpdateStringPacket(row.data)
-
-        parsers.eventUpdateStringPacket(row, p)
-    end,
-
-    [0x065] = function (row)
-        local p = xiffi.toPositionPacket(row.data)
-
-        parsers.positionPacket(row, p)
-    end,
+    [0x065] =  parsers.positionPacket,
 }
 
 local oParsers = {
+
+}
+
+local iPacketTypes = {
+    [0x00A] = xiffi.toZoneInPacket,
+
+    [0x00E] = xiffi.toEntityUpdatePacket,
+
+    [0x032] = xiffi.toEventPacket,
+    [0x033] = xiffi.toEventStringPacket,
+    [0x034] = xiffi.toEventParamPacket,
+
+    [0x052] = xiffi.toReleasePacket,
+
+    [0x057] = xiffi.toWeatherPacket,
+
+    [0x05B] = xiffi.toPositionPacket,
+    [0x05C] = xiffi.toEventUpdatePacket,
+    [0x05D] = xiffi.toEventUpdateStringPacket,
+
+    [0x065] = xiffi.toPositionPacket,
+}
+
+local oPacketTypes = {
 
 }
 
@@ -94,17 +70,51 @@ if not (ret and db) then
     return
 end
 
-for row in db:nrows('SELECT created_at, kind, type, data FROM entry WHERE kind IN (4,5) ORDER BY ticks') do
-    local f = nil
-
+-- example filter for testing
+local function filter(row, p)
     if row.kind == 4 then
-        f = iParsers[row.type]
-    elseif row.kind == 5 then
-        f = oParsers[row.type]
+        if row.type == 0x00A then
+            return true
+        end
+
+        if row.type == 0x00E then
+            ---@cast p EntityUpdatePacket
+            if p.UpdateFlags04 and p.RequiredEntity then
+                return true
+            end
+        end
     end
 
-    if f ~= nil then
-        f(row)
+    return false
+end
+
+for row in db:nrows('SELECT created_at, kind, type, data FROM entry WHERE kind IN (4,5) ORDER BY ticks') do
+    local input = false
+    local conv = nil
+
+    if row.kind == 4 then
+        -- server to client
+        input = true
+        conv = iPacketTypes[row.type]
+    elseif row.kind == 5 then
+        -- client to server
+        conv = oPacketTypes[row.type]
+    end
+
+    if conv ~= nil then
+        local p = conv(row.data)
+
+        if filter(row, p) then
+            if row.kind == 4 then
+                f = iParsers[row.type]
+            elseif row.kind == 5 then
+                f = oParsers[row.type]
+            end
+
+            if f ~= nil then
+                f(row, p)
+            end
+        end
     end
 end
 
